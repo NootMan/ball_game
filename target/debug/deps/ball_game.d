2d12e174d1fbe963898e05e611292ad2f9636c5d
Tristian Quijas <quijast@gmail.com>
2023-11-24 13:17:01 -0800

    file location change?
    not really sure whats going on here

--- target/debug/deps/ball_game.d
+++ target/debug/deps/ball_game.d
@@ -1,5 +1,5 @@
-C:\Users\quija\Desktop\Dev Shit\bevy ball game\ball_game\target\debug\deps\ball_game.exe: src\main.rs
+D:\quija\Random Shit\dev shit\Rust learning shit\NEWBevyBallGame\ball_game\target\debug\deps\ball_game.exe: src\main.rs
 
-C:\Users\quija\Desktop\Dev Shit\bevy ball game\ball_game\target\debug\deps\ball_game.d: src\main.rs
+D:\quija\Random Shit\dev shit\Rust learning shit\NEWBevyBallGame\ball_game\target\debug\deps\ball_game.d: src\main.rs
 
 src\main.rs:
