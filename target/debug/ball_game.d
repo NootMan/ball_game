d71c79b3a0793b300d1983756263dccb16a5bc6a
Tristian Quijas <quijast@gmail.com>
2023-11-24 12:46:37 -0800

    changed git ignore
    i remove target from gitignore, i'm hoping it makes cargo work on my laptop lol

---
+++ target/debug/ball_game.d
@@ -0,0 +1,1 @@
+D:\quija\Random\ Shit\dev\ shit\Rust\ learning\ shit\NEWBevyBallGame\ball_game\target\debug\ball_game.exe: D:\quija\Random\ Shit\dev\ shit\Rust\ learning\ shit\NEWBevyBallGame\ball_game\src\main.rs
---
+++ target/debug/deps/ball_game-5a8b268a87d670c1.d
@@ -0,0 +1,5 @@
+d:\quija\Random Shit\dev shit\Rust learning shit\NEWBevyBallGame\ball_game\target\debug\deps\ball_game-5a8b268a87d670c1.rmeta: src\main.rs
+
+d:\quija\Random Shit\dev shit\Rust learning shit\NEWBevyBallGame\ball_game\target\debug\deps\ball_game-5a8b268a87d670c1.d: src\main.rs
+
+src\main.rs:
